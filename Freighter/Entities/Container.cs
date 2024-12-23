020405fc6b3affd20bf4c510ee3bf3e15b870896
CaioCDJ <caiodjesus1@hotmail.com>
2024-12-22 22:13:28 -0300

    fix(containers): start, stop and delete

--- Freighter/Entities/Container.cs
+++ Freighter/Entities/Container.cs
@@ -1,13 +1,29 @@
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Numerics;
+using System.Reactive;
 using Docker.DotNet.Models;
+using ReactiveUI;
 
 namespace Freighter.Entities;
 
 public class Container {
-	public string id { get; set; }
-	public string name { get; set; }
-	public string image_name { get; set; }
-	public IList<Port> ports { get; set; }
-	public string state { get; set; }
-	public string status_message { get; set; }
+	public string id { get; init; }
+	public string name { get; init; }
+	public string image_name { get; init; }
+	public IList<Port> ports { get; init; }
+	public string state { get; init; }
+	public string status_message { get; init; }
+	
+	public bool is_running { get; init; }
+
+	public bool not_running {
+		get => !is_running;
+		init => is_running = !value;
+	}
+	
+	public  ReactiveCommand<string,Unit> start_command{ get; init; }
+	public  ReactiveCommand<string,Unit> stop_command{ get; init; }
+	public  ReactiveCommand<string,Unit> delete_command{ get; init; }
+	
 }
--- Freighter/ViewModels/ContainersPageViewModel.cs
+++ Freighter/ViewModels/ContainersPageViewModel.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Threading;
 using System.Threading.Tasks;
 using Avalonia.Media.Immutable;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Docker.DotNet;
 using Docker.DotNet.Models;
 using DynamicData;
@@ -13,8 +17,8 @@ using ReactiveUI;
 namespace Freighter.ViewModels;
 
 public class ContainersPageViewModel : ReactiveObject, IRoutableViewModel {
-
 	public IScreen HostScreen { get; }
+
 	// Unique identifier for the routable view model.
 	public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
 
@@ -22,19 +26,30 @@ public class ContainersPageViewModel : ReactiveObject, IRoutableViewModel {
 
 	public ObservableCollection<Container> containers { get; set; }
 
+	private ReactiveCommand<string, Unit> start_command { get; set; }
+	private ReactiveCommand<string, Unit> stop_command { get; set; }
+	private ReactiveCommand<string, Unit> delete_command { get; set; }
 
+	private ReactiveCommand<Unit, Unit> refresh { get; set; }
+
+	private Func<Task> refreshAction { get; set; }
+	
 	public ContainersPageViewModel(IScreen hostScreen) {
 		HostScreen = hostScreen;
 
+		refreshAction = async () => await refresh_data();
+		
+		start_command = ReactiveCommand.CreateFromTask<string>(onStartContainer);
+		stop_command = ReactiveCommand.CreateFromTask<string>(onStopContainer);
+		delete_command = ReactiveCommand.CreateFromTask<string>(onDeleteContainer);
+		
 		containers = new ObservableCollection<Container>();
 
 		_docker_client = new DockerClientConfiguration(
 				new Uri("unix:///var/run/docker.sock"))
 			.CreateClient();
-
-		var items = _docker_client.Containers.ListContainersAsync(new ContainersListParameters() {
-			Limit = 10,
-		}).Result;
+		var items = _docker_client.Containers.ListContainersAsync(new ContainersListParameters() { Limit = 10, })
+			.Result;
 
 		foreach (var item in items) {
 			containers.Add(new Container() {
@@ -42,8 +57,12 @@ public class ContainersPageViewModel : ReactiveObject, IRoutableViewModel {
 				name = item.Names[0].Replace("/", ""),
 				state = item.State,
 				image_name = item.Image,
-				status_message = item.Status,
-				ports = item.Ports
+				status_message = item.State,
+				is_running = item.State.Contains("running"),
+				ports = item.Ports,
+				stop_command = stop_command,
+				start_command = start_command,
+				delete_command = delete_command,
 			});
 		}
 	}
@@ -51,9 +70,7 @@ public class ContainersPageViewModel : ReactiveObject, IRoutableViewModel {
 	public async Task refresh_data() {
 		containers.Clear();
 
-		var items = await _docker_client.Containers.ListContainersAsync(new ContainersListParameters() {
-			Limit = 10,
-		});
+		var items = await _docker_client.Containers.ListContainersAsync(new ContainersListParameters() { Limit = 10, });
 
 		foreach (var item in items) {
 			containers.Add(new Container() {
@@ -61,9 +78,53 @@ public class ContainersPageViewModel : ReactiveObject, IRoutableViewModel {
 				name = item.Names[0].Replace("/", ""),
 				state = item.State,
 				image_name = item.Image,
-				status_message = item.Status,
-				ports = item.Ports
+				status_message = item.State,
+				is_running = item.State.Contains("running"),
+				ports = item.Ports,
+				stop_command = stop_command,
+				start_command = start_command,
+				delete_command = delete_command,
 			});
 		}
+	}
+
+	private async Task onStartContainer(string id) {
+
+		var container = containers.SingleOrDefault(x => x.id == id);
+		
+		if (container is null) {}
+
+		await _docker_client.Containers.StartContainerAsync(
+			id,
+			new ContainerStartParameters());
+
+		await refresh_data();
+	}
+
+	private async Task onStopContainer(string id) {
+		var container = containers.SingleOrDefault(x => x.id == id);
+		
+		if (container is null) {}
+		
+		await _docker_client.Containers.StopContainerAsync(
+			id,
+			new ContainerStopParameters());
+		await refresh_data();
+	}
+
+	private async Task onDeleteContainer(string id) {
+		
+		var container = containers.SingleOrDefault(x => x.id == id);
+		
+		if (container is null) {}
+
+		if (container.is_running) {
+			await onStopContainer(id);
+		}
+
+		await	_docker_client.Containers.RemoveContainerAsync(
+			id, new ContainerRemoveParameters());
+
+		await refresh_data();
 	}
 }
